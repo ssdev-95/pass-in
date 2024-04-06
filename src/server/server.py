47442ed482548bee526e708b72e197f468c98339
SalomaoS <souza95salomao@gmail.com>
2024-04-06 20:51:33 +0000

    chore: update github actions ci workflows

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 
-from .routes.events_routes import events_routes_bp
+from .routes import events_routes, attendees_routes, swagger_ui
 from src.models.settings.connection import db_connection_handler
+
+blueprints = [
+    attendees_routes.blueprint,
+    events_routes.blueprint,
+    swagger_ui.blueprint
+]
 
 def create_app():
     db_connection_handler.connect_to_db()
@@ -10,6 +16,7 @@ def create_app():
     flask_app = Flask(__name__)
     CORS(flask_app)
 
-    flask_app.register_blueprint(events_routes_bp)
+    for blueprint in blueprints:
+        flask_app.register_blueprint(blueprint)
 
     return flask_app

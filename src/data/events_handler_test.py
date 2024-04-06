5f17a59c5ca5cdbc3ba8d24f08859b410636c73e
SalomaoS <souza95salomao@gmail.com>
2024-04-06 13:27:48 +0000

    feat: events_handler tests

--- src/data/events_handler_test.py
+++ src/data/events_handler_test.py
@@ -4,7 +4,6 @@ from .attendees_handler import AttendeesHandler
 from init.test_mocks import create_bulk_attendees_mocks, create_event_mock
 from src.models.settings.connection import db_connection_handler
 
-
 db_connection_handler.connect_to_db()
 events_handler = EventsHandler()
 
@@ -16,15 +15,20 @@ event_id = ''
 def test_create_event():
     http_request = HTTPRequest(body=mock_event)
     http_response = events_handler.create_event(http_request)
-    event_id = http_response.body.get('event_id')
+
+    global event_id
+    event_id = http_response.body.get('eventId')
+    print(event_id)
 
 
 def test_find_by_id():
+    print(event_id)
     http_request = HTTPRequest(params={'event_id':event_id})
     events_handler.find_by_id(http_request)
 
 
 def test_get_attendees_from_event():
+    print(event_id)
     attendees_handler = AttendeesHandler()
 
     for attendee in attendees_mocks:
